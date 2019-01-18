9fbdff8148e08bade397e44ca6ef73722c138e4e
YdeaGames <ydeagames@gmail.com>
2019-01-18 12:42:06 +0900

    Game Assets

---
+++ Assets/Liquid2D/LiquidGenarate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiquidGenarate : MonoBehaviour {
+	public int _numLiquid = 10;
+	public GameObject _prefab;
+	
+	// Use this for initialization
+	void Start () {
+        var col = GetComponent<BoxCollider2D>();
+		for (int i = 0; i < _numLiquid; i++) {
+			GameObject temp = Instantiate(_prefab) as GameObject;
+			float x = Random.Range(col.bounds.min.x, col.bounds.max.x);
+			float y = Random.Range(col.bounds.min.y, col.bounds.max.y);
+			temp.transform.localPosition = new Vector3 (x, y, 0);
+			temp.transform.parent = transform;
+		}
+	}
+}
