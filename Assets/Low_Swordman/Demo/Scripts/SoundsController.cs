935e4d666c92934d0c8e028d1555068726198ba0
kkmsgr <ygcxkr4mg@i.softbank.jp>
2019-01-25 12:19:56 +0900

    ステージの縮小・看板の設置

---
+++ Assets/Low_Swordman/Demo/Scripts/SoundsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundsController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //イントロ部分の再生開始
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
