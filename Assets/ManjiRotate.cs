6aff8a46d6d5d076e9fae4bae9d89ccefc2056e8
kkmsgr <ygcxkr4mg@i.softbank.jp>
2019-01-31 13:23:13 +0900

    まんじ

---
+++ Assets/ManjiRotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManjiRotate : MonoBehaviour
+{
+    public float speed = 5;
+    
+    void Update()
+    {
+        Rigidbody2D rigid = GetComponent<Rigidbody2D>();
+        rigid.AddTorque(speed * Time.deltaTime);
+    }
+}
